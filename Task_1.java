ca89d2c586a236c07dfb3557e5d8ec331d784fe1
Aleksey <J2k_pot@mail.ru>
2023-02-06 19:18:06 +0300

    add 1

--- Task_1.java
+++ Task_1.java
@@ -12,15 +12,16 @@ public class Task_1 {
         System.out.println("Обратный LinkedList: " + rev);
     }
 
-    public static LinkedList<Integer> FillList(int size, int max){
+    public static LinkedList<Integer> FillList(int size, int max) {
         LinkedList<Integer> defList = new LinkedList<>();
         Random rand = new Random();
-        for (int i = 0; i < size ; i++) {
+        for (int i = 0; i < size; i++) {
             defList.add(rand.nextInt(max));
         }
         return defList;
     }
-    public static LinkedList<Integer> NewRevertedList(LinkedList<Integer> listToRevert){
+
+    public static LinkedList<Integer> NewRevertedList(LinkedList<Integer> listToRevert) {
         LinkedList<Integer> newList = new LinkedList<>();
         int size = listToRevert.size();
         for (int i = size - 1; i >= 0; i--) {
@@ -28,11 +29,12 @@ public class Task_1 {
         }
         return newList;
     }
-    public static void RevertList(LinkedList<Integer> listToRevert){
+
+    public static void RevertList(LinkedList<Integer> listToRevert) {
         int size = listToRevert.size();
         int distance = size;
         int i = 0;
-        while (distance > 1){
+        while (distance > 1) {
             int tmp = listToRevert.get(i);
             listToRevert.set(i, listToRevert.get(size - i - 1));
             listToRevert.set(size - i - 1, tmp);
@@ -40,7 +42,8 @@ public class Task_1 {
             distance = size - 2 * i;
         }
     }
-    public static void RevertListColl(LinkedList<Integer> listToRevert){
+
+    public static void RevertListColl(LinkedList<Integer> listToRevert) {
         Collections.reverse(listToRevert);
     }
 }
